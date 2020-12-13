4fc8ad93ba21eafcb970e583fe5a647111632bc2
Sami Khalaf <samifalafel>
2020-12-13 21:02:44 +0100

    pokemon fetched in the home page, only in console

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,31 @@
 export default () => {
-    const views = "<h1>Esto es la home</h1>";
-  
-    const divElement = document.createElement("div");
-    divElement.innerHTML = views;
-  
-    return divElement;
-  };
-  
+  const API_BASE_URL = "https://pokeapi.co/api/v2/";
+
+  window.onload = function () {};
+
+  fetch(API_BASE_URL + "pokemon")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      let pokemons = data.results;
+      console.log(pokemons);
+
+      pokemons.map((pokemon) => {
+        console.log(pokemon.url);
+        fetch(pokemon.url)
+        .then((res) => res.json()
+        .then((specificPokemon) => console.log(specificPokemon))
+
+        
+        );
+      });
+    });
+
+  const views = "<h1>Esto es la home</h1>";
+
+  const divElement = document.createElement("div");
+  divElement.innerHTML = views;
+
+  return divElement;
+};
